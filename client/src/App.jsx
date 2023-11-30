28ead573aa8919d3d698dc5691e46478e8832e8b
Sahitya Neupane <neupanesahitya1@gmail.com>
2023-11-30 09:23:47 -0500

    changes the link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
-// import React from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+
 const App = () => {
-  return <h1 className="text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default App;
